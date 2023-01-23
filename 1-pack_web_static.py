cc9c1bea258783503e79f640a7626956f5ff8dc0
Lourdel <klourdel@gmail.com>
2023-01-23 19:34:12 +0000

    compress

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+"""
+from datetime import datetime
+from fabric.api import local
+import os
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        time = datetime.now()
+        fmt = "%Y%m%d%H%M%S"
+        file_path = 'versions/web_static_{}.tgz'.format(time.strftime(fmt))
+        local('tar - cvzf {} web_static'.format(file_path))
+        return file_path
+    except:
+        return None
