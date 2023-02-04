99f1d3fd6fa4a422f8d7251144f9ac8c3bc1ff49
Lourdel <klourdel@gmail.com>
2023-02-04 12:31:59 +0000

    flask routes and jinja templates

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+script that starts a flas web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """method to display """
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def Hbnb():
+    """Display HBNH"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_route(text):
+    """display “C ” followed by the value of the text variable"""
+    return ("C " + text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pyiscool(text="is cool"):
+    """
+    display “Python ”, followed by the value of the text variable
+    """
+    return ("Python " + text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def Number(n):
+    """
+    display “n is a number” only if n is an integer
+    """
+    return f"{n} is a number\n"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
